8395787c651ee424ddbd28a91c49c5c76ec8fe30
RadMom <RadMom1111@gmail.com>
2023-10-01 16:47:49 +0300

    Controllers redy. Need final check and polish

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -6,7 +6,8 @@ const authUser = require("../middleware/authMiddleware");
 // createTask
 // tasksRoutes.post("/");
 const createTask = async (req, res) => {
-    const { task, userId } = req.body;
+    const task = req.body;
+    const userId = req.user._id;
     console.log(req.body);
 
     // if (task.title  || !task.deadline || task.description) {
@@ -79,12 +80,40 @@ const getTask = async (req, res) => {
 // editTask
 // tasksRoutes.put("/:taskId");
 const editTask = async (req, res) => {
-    const taskId = req.params.taskId;
-    const userId = req.user;
-    const task = req.body;
+    const taskId = req.params.taskId; // Extract the task ID from the request parameters
+    const userId = req.user._id;
+    console.log("TypeOf userId" + typeof userId);
+    const { title, description, deadline, status, isPublic, priority } = req.body;
     try {
+        // Check if the task exists
+        const task = await Task.findById(taskId);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        // Check if the user is the creator of the task
+        if (task.creator.toString() !== userId.toString()) {
+            return res
+                .status(403)
+                .json({ message: "You do not have permission to edit this task" });
+        }
+
+        // Update task properties
+        if (title) task.title = title;
+        if (description) task.description = description;
+        if (deadline) task.deadline = deadline;
+        if (status) task.status = status;
+        if (isPublic !== undefined) task.isPublic = isPublic;
+        if (priority) task.priority = priority;
+
+        // Save the updated task
+        await task.save();
+
+        return res.status(200).json({ message: "Task updated successfully" });
     } catch (err) {
-        console.log(err);
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
     }
 };
 
